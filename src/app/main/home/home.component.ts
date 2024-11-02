c599392c827e2073fca427c8b0824502a508fa87
daniel868 <sincalexandrudaniel@gmail.com>
2024-11-02 16:11:04 +0200

    add home components

---
+++ src/app/main/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {ReactiveFormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
